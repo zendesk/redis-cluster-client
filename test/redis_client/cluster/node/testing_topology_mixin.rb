0a012c8a8f4a8a414a330cbdcf702609f3947df5
KJ Tsanaktsidis <ktsanaktsidis@zendesk.com>
2023-12-18 09:52:33 +1100

    Fix the topology tests

--- test/redis_client/cluster/node/testing_topology_mixin.rb
+++ test/redis_client/cluster/node/testing_topology_mixin.rb
@@ -5,33 +5,20 @@ class RedisClient
     class Node
       module TestingTopologyMixin
         def setup
-          test_config = ::RedisClient::ClusterConfig.new(
+          @test_config = ::RedisClient::ClusterConfig.new(
             nodes: TEST_NODE_URIS,
             fixed_hostname: TEST_FIXED_HOSTNAME,
+            **self.class::TESTING_TOPOLOGY_OPTIONS,
             **TEST_GENERIC_OPTIONS
           )
           @concurrent_worker = ::RedisClient::Cluster::ConcurrentWorker.create
-          if TEST_FIXED_HOSTNAME
-            test_node_info_list = ::RedisClient::Cluster::Node.new(@concurrent_worker, config: test_config).node_info
-            test_node_info_list.each do |info|
-              _, port = ::RedisClient::Cluster::NodeKey.split(info.node_key)
-              info.node_key = ::RedisClient::Cluster::NodeKey.build_from_host_port(TEST_FIXED_HOSTNAME, port)
-            end
-            node_addrs = test_node_info_list.map { |info| ::RedisClient::Cluster::NodeKey.hashify(info.node_key) }
-            test_config = ::RedisClient::ClusterConfig.new(
-              nodes: node_addrs,
-              fixed_hostname: TEST_FIXED_HOSTNAME,
-              **TEST_GENERIC_OPTIONS
-            )
-          end
-          test_node = ::RedisClient::Cluster::Node.new(@concurrent_worker, config: test_config)
-          @options = test_node.instance_variable_get(:@node_configs)
-          @replications = test_node.instance_variable_get(:@replications)
-          @test_topology = test_node.instance_variable_get(:@topology)
+          @test_node = ::RedisClient::Cluster::Node.new(@concurrent_worker, config: @test_config)
+          @replications = @test_node.instance_variable_get(:@replications)
+          @test_topology = @test_node.instance_variable_get(:@topology)
         end
 
         def teardown
-          @test_topology&.clients&.each_value(&:close)
+          @test_node&.each { |c| c.close }
         end
       end
     end
